0759ffcfe77eba36a7cd430ebe547211f54c1338
n1cef1sh <757237488@qq.com>
2020-10-10 19:29:27 +0800

    1

---
+++ wechat/src/components/chatcom.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="chat">
+        <h1 class="redbg">用户：{{currentUser.username}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['currentUser']
+}
+</script>
+
+<style scoped>
+    .chat{
+        width: 800px;
+        height: 700px;
+        border: 1px solid #ccc;
+    }
+</style>
