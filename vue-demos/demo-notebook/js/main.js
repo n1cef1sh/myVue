43547a083d548f36e5154c96941f74a123ff3d81
n1cef1sh <757237488@qq.com>
2020-10-13 10:55:14 +0800

    update

---
+++ vue-demos/demo-notebook/js/main.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el: "#todoapp",
+    data: {
+      //  总数据
+      todoList: ["吃饭饭", "睡觉觉", "写代码"],
+      //  输入的内容
+      inputValue: "",
+    },
+    // 方法
+    methods: {
+      // 增加任务
+      addTodo() {
+        this.todoList.push(this.inputValue);
+      },
+      // 删除任务
+      delTodo(index) {
+        this.todoList.splice(index, 1);
+      },
+      // 清空内容
+      clearTodo() {
+        this.todoList = [];
+      }
+    }
+  });
+  
