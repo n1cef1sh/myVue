2a8b3f2e10729308507ea65e91732783a0170ff4
n1cef1sh <757237488@qq.com>
2020-10-14 11:11:28 +0800

    update

--- vue-demos/demo-musicplayer/js/main.js
+++ vue-demos/demo-musicplayer/js/main.js
@@ -52,10 +52,38 @@ var app = new Vue({
             var that = this;
             axios.get('https://autumnfish.cn/search?keywords='+this.query)
             .then(function(response){
-                //console.log(response);
+                console.log(response);
                 that.musicList = response.data.result.songs;
-            })
-            .catch(function(err){})
+                console.log(response.data.result.songs);
+            },
+            function(err){})
+        },
+        playMusic: function(musicId){
+          //console.log(musicId);
+          var that = this;
+          axios.get("https://autumnfish.cn/song/url?id="+musicId)
+          .then(function(response){
+            //console.log(response);
+            //console.log(response.data.data[0].url);
+            that.musicUrl = response.data.data[0].url;
+          },
+          function(err){})
+
+          //歌曲详情
+          axios.get("https://autumnfish.cn/song/detail?ids="+musicId)
+          .then(function(response){
+            //console.log(response);
+            console.log(response.data.songs[0].al.picUrl);
+            that.musicCover = response.data.songs[0].al.picUrl;
+          },function(err){})
+
+          //歌曲评论
+          axios.get("https://autumnfish.cn/comment/hot?type=0?id="+musicId)
+          .then(function(response){
+            //console.log(response);
+            console.log(response.data.hotComments);
+            that.hotComments = response.data.hotComments;
+          },function(err){})
         }
     }
 })
